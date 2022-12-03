d91e7bff526d530d24e72dea04c39e8de5c3bb2c
Tim Dowling <timothy.dowling@me.com>
2022-12-02 21:06:30 -0500

    Day 2 Completed

--- 2022/AOC2022/AOC2022/Days/Day2.cs
+++ 2022/AOC2022/AOC2022/Days/Day2.cs
@@ -25,7 +25,7 @@ namespace AOC2022.Days
         }
         private void _ReadData()
         {
-            string[] data = AOCTools.GetData("Day2_ex");
+            string[] data = AOCTools.GetData("Day2");
 
             foreach (string line in data)
             {
@@ -35,12 +35,113 @@ namespace AOC2022.Days
 
         private void _Part1()
         {
+            int totalScore = 0;
+
+            foreach (var r in _rounds)
+            {
+                totalScore += _PlayRound(r[0], r[1]);
+            }
+
+            AOCTools.Log("Part 1");
+            AOCTools.Log("Total Score: " + totalScore);
 
         }
 
         private void _Part2()
         {
+            int totalScore = 0;
 
+            foreach (var r in _rounds)
+            {
+                totalScore += _PlayRoundCorrectly(r[0], r[1]);
+            }
+
+            AOCTools.Log("Part 2");
+            AOCTools.Log("Total Score: " + totalScore);
+        }
+
+        // Part1 play round
+        private int _PlayRound(string elf, string you)
+        {
+            switch (elf)
+            {
+                case "A":
+                    switch (you)
+                    {
+                        case "X":
+                            return 1 + 3;
+                        case "Y":
+                            return 2 + 6;
+                        case "Z":
+                            return 3 + 0;
+                    }
+                    break;
+                case "B":
+                    switch (you)
+                    {
+                        case "X":
+                            return 1 + 0;
+                        case "Y":
+                            return 2 + 3;
+                        case "Z":
+                            return 3 + 6;
+                    }
+                    break;
+                case "C":
+                    switch (you)
+                    {
+                        case "X":
+                            return 1 + 6;
+                        case "Y":
+                            return 2 + 0;
+                        case "Z":
+                            return 3 + 3;
+                    }
+                    break;
+            }
+            return -1;
+        }
+
+        //Part2 Play round
+        private int _PlayRoundCorrectly(string elf, string you)
+        {
+            switch (elf)
+            {
+                case "A":
+                    switch (you)
+                    {
+                        case "X":
+                            return 3 + 0;
+                        case "Y":
+                            return 1 + 3;
+                        case "Z":
+                            return 2 + 6;
+                    }
+                    break;
+                case "B":
+                    switch (you)
+                    {
+                        case "X":
+                            return 1 + 0;
+                        case "Y":
+                            return 2 + 3;
+                        case "Z":
+                            return 3 + 6;
+                    }
+                    break;
+                case "C":
+                    switch (you)
+                    {
+                        case "X":
+                            return 2 + 0;
+                        case "Y":
+                            return 3 + 3;
+                        case "Z":
+                            return 1 + 6;
+                    }
+                    break;
+            }
+            return -1;
         }
     }
 }
