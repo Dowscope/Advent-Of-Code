d67e6b8b7ecb2d9447d3b7fcc1071e76c6eb4ff9
Tim Dowling <timothy.dowling@me.com>
2022-12-05 23:07:09 -0500

    Stuck on 5

---
+++ 2022/AOC2022/AOC2022/Days/Day5.cs
@@ -0,0 +1,109 @@
+﻿using AOC2022.Utils;
+
+namespace AOC2022.Days
+{
+    internal class Day5 : Day
+    {
+        public Day5() 
+        {
+            AOCTools.Log("AOC2022 - Day 5");
+            ReadData("Day5");
+            if (data.Length != 0)
+            {
+                Part1();
+                Part2();
+            }
+            else
+            {
+                AOCTools.Log("Data file empty");
+            }
+        }
+
+        public override void Part1()
+        {
+            Dictionary<int, Stack<string>> stacks = new Dictionary<int, Stack<string>>();
+            bool isInstructions = false;
+            int stackedColumns = 0;
+
+            for (int i = 0; i < data.Length; i++ )
+            {
+                var line = data[i];
+
+                if (!isInstructions)
+                {
+                    if (line == "")
+                    {
+                        isInstructions = true;
+                        for (int j = i - 1; j >= 0; j--)
+                        {
+                            var data_c = data[j].Trim();
+                            var data_r = data[j].Replace(" ", ",");
+                            var row = data[j].Split(',');
+                            if (j == i - 1)
+                            {
+                                string col = "";
+                                foreach (var r in row)
+                                {
+                                    if (r != "")
+                                    {
+                                        col += r;
+                                    }
+                                }
+                                stackedColumns = col.Length;
+                                continue;
+                            }
+                            for (int k = 1; k <= stackedColumns; k++) 
+                            {
+                                var c = row[k-1].Replace("[", "").Replace("]","").Replace(" ", "");
+
+                                if (c == "" || c == " ")
+                                {
+                                    continue;
+                                }
+
+                                Stack<string> value = new Stack<string>();
+                                if (stacks.TryGetValue(k, out value))
+                                {
+                                    stacks[k].Push(c);
+                                }
+                                else
+                                {
+                                    Stack<string> v = new Stack<string>();
+                                    v.Push(c);
+                                    stacks.Add(k, v);
+                                }
+                            }
+                        }
+                        continue;
+                    }
+
+
+                }
+                else
+                {
+                    /*string[] commands = line.Split();
+
+                    int quanity = Int32.Parse(commands[1]);
+                    int from = Int32.Parse(commands[3]);
+                    int to = Int32.Parse(commands[5]);
+
+                    for (int j = 0; j < quanity; j++)
+                    {
+                        string toMove = stacks[from].Pop();
+                        stacks[to].Push(toMove);
+                    }*/
+                }
+            }
+
+            AOCTools.Log("\nPart 1");
+            foreach (var result in stacks.Values)
+            {
+                AOCTools.Log(result.Peek());
+            }
+        }
+
+        public override void Part2()
+        {
+        }
+    }
+}
--- 2022/AOC2022/AOC2022/Program.cs
+++ 2022/AOC2022/AOC2022/Program.cs
@@ -8,4 +8,5 @@ using AOC2022.Days;
 // Day1 day1 = new Day1();
 // Day2 day2 = new Day2();
 // Day3 day3 = new Day3();
-Day4 day4= new Day4();
+// Day4 day4 = new Day4();
+Day5 day5 = new Day5();
