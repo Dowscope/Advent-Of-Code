cfa5fb48112767760c2b754f028006de359bd89c
Timothy Dowling <timothy.dowling@me.com>
2022-12-08 20:23:55 -0500

    Update

--- 2022/AOC2022/AOC2022/Days/Day7.cs
+++ 2022/AOC2022/AOC2022/Days/Day7.cs
@@ -10,16 +10,117 @@ namespace AOC2022.Days
 
         public override void Part1()
         {
+            int level = 0;   // 0 is root level
+            Folder currentFolder = new Folder("/", 0);
 
             foreach (var line in data)
             {
+                var words = line.Split();
 
+                if (words[0] == "$")
+                {
+                    if (words[1] == "cd")
+                    {
+                        if (words[2] == "/")
+                        {
+                            currentFolder = _ReturnToRoot(currentFolder, level);
+                            level = 0;
+                        }
+                        else if (words[2] == "..")
+                        {
+                            if (level > 0 && currentFolder != null)
+                            {
+                                currentFolder = currentFolder.Parent;
+                                level--;
+                            }
+                        }
+                        else
+                        {
+                            currentFolder = currentFolder.GetFolder(words[2]);
+                            if (currentFolder == null)
+                            {
+                                AOCTools.Log("Invalid Folder: " + words[2]);
+                                break;
+                            }
+                            level++;
+                        }
+                    }
+                }
+                else if (words[0] == "dir")
+                {
+                    if (currentFolder != null && !currentFolder.Exists("d", words[1]))
+                    {
+                        currentFolder.AddFolder(words[1]);
+                    }
+                }
+                else
+                {
+                    if (currentFolder != null && !currentFolder.Exists("f", words[1]))
+                    {
+                        currentFolder.AddFile(words[1], words[0]);
+                    }
+                }
             }
+
+            currentFolder = _ReturnToRoot(currentFolder, level);
+            level = 0;
+            _TotalSizeOfFolderUpdate(currentFolder);
+            _GetTotalSizes(currentFolder);
+            long total = 0;
+            foreach (var t in _totals)
+            {
+                total += t;
+            }
+            AOCTools.Log("Total File Size: " + total);
         }
 
         public override void Part2()
         {
 
+        }
+
+        private void _TotalSizeOfFolderUpdate(Folder current)
+        {
+            if (current.Folders != null)
+            {
+                foreach (var f in current.Folders)
+                {
+                    _TotalSizeOfFolderUpdate(f);
+                }
+            }
+
+            if (current.Parent != null)
+            {
+                current.Parent.TotalFileSize += current.TotalFileSize;
+            }
+        }
+
+        private List<long> _totals = new List<long>();
+        private void _GetTotalSizes(Folder current)
+        {
+            if (current.TotalFileSize > 100000 && current.Folders != null)
+            {
+                foreach (var f in current.Folders)
+                {
+                    _GetTotalSizes(f);
+                }
+            }
+
+            if (current.TotalFileSize <= 100000)
+            {
+                _totals.Add(current.TotalFileSize);
+            }
+        }
+
+        private Folder _ReturnToRoot(Folder current, int level)
+        {
+            while (level > 0)
+            {
+                current = current.Parent;
+                level--;
+            }
+
+            return current;
         }
     }
 }
--- 2022/AOC2022/AOC2022/Utils/Folder.cs
+++ 2022/AOC2022/AOC2022/Utils/Folder.cs
@@ -4,31 +4,74 @@ namespace AOC2022.Utils
 {
     internal class Folder
     {
-        private List<Folder> _folders;
         private List<string[]> _files;
 
-        public long GetTotalFileSize 
-        { 
-            get
-            {
-                long total = 0;
-                foreach (var file in _files)
-                {
-                    total += long.Parse(file[1]);
-                }
-                return total;
-            } 
+        public List<Folder> Folders { get; private set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public Folder Parent { get; set; }
+        public long TotalFileSize { get; set; }
+
+        public Folder(string name, int level, Folder? parent = null)
+        {
+            Name = name;
+            Level = level;
+            Parent = parent;
         }
 
-        public Folder(string name)
+        public void AddFile(string name, string size)
         {
-
-        }
-
-        public void AddFile(string name, long size)
-        {
-            string[] s = {name, size };
+            if (_files == null) _files = new List<string[]>();
             _files.Add(new[] { name, size });
+            TotalFileSize += long.Parse(size);
+        }
+
+        public void AddFolder(string name)
+        {
+            if (Folders == null) Folders = new List<Folder>();
+            Folders.Add(new Folder(name, Level + 1, this));
+        }
+
+        public Folder? GetFolder(string name)
+        {
+            if (Folders != null)
+            {
+                foreach(var f in Folders)
+                {
+                    if(f.Name == name)
+                    {
+                        return f;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public bool Exists(string type, string name)
+        {
+            if (type == "d" && Folders != null)
+            {
+                foreach (var f in Folders)
+                {
+                    if (f.Name == name)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else if (type == "f" && _files != null)
+            {
+                foreach (var f in _files)
+                {
+                    if (f[0] == name)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return false;
         }
     }
 }
