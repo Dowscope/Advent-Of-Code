eebf729dd0e37be5c9bc2b84f036fdd17e3b6868
Timothy Dowling <timothy.dowling@me.com>
2023-12-02 21:29:21 -0500

    Day 1 setup

--- 2023/Days/Day1/day1.js
+++ 2023/Days/Day1/day1.js
@@ -1,13 +1,47 @@
 const fs = require('fs')
 
-const test_filePath = './test.txt'
+const test_filePath = './Days/Day1/test.txt'
 
 function run() {
   console.log("Day 1")
+  const data = loadFile(test_filePath)
+
+  if (data == null){
+    console.log('Failed reading file')
+    return
+  }
+
+  const result = [
+    {'Part 1': partone(data)}
+  ]
+
+  displayResults(result)
 }
 
-function loadFiles() {
+function partone(data){
+  const lines = data.split('\n')
+
   
+
+  return lines.length
+}
+
+function displayResults(data){
+  console.log("The Results")
+  console.log("-----------")
+  console.log(data)
+}
+
+function loadFile(path) {
+  process.stdout.write(`Reading File: ${path}... `)
+  try {
+    const data = fs.readFileSync(path, 'utf8')
+    console.log("Successful")
+    return data
+  } catch (error) {
+    console.log(`Failed\nError reading file ${path}. \n${error}`)
+    return null
+  }
 }
 
 module.exports = {
