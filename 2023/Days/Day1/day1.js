12f2c5b6d8a04b5027d240429815d4cf872b2706
Timothy Dowling <timothy.dowling@me.com>
2023-12-03 22:12:26 -0500

    Day One Complete

--- 2023/Days/Day1/day1.js
+++ 2023/Days/Day1/day1.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
 
-const test_filePath = './Days/Day1/test.txt'
+const reset = "\x1b[0m";
+const red = "\x1b[31m";
+const yellow = "\x1b[38;5;226m";
+
+// const test_filePath = './Days/Day1/test.txt'
+// const test_filePath = './Days/Day1/testb.txt'
+const test_filePath = './Days/Day1/data.txt'
 
 function run() {
   console.log("Day 1")
@@ -12,18 +18,63 @@ function run() {
   }
 
   const result = [
-    {'Part 1': partone(data)}
+    {'Part 1': partone(data)},
+    {'Part 2': parttwo(data)}
   ]
 
   displayResults(result)
 }
 
 function partone(data){
+  console.log(yellow + "Part One" + reset)
   const lines = data.split('\n')
 
-  
+  var result = 0
 
-  return lines.length
+  for (var l of lines) {
+    const numbers = l.match(/\d/g)
+
+    console.log(`Line OG: ${l}`)
+    if (numbers != null){
+      const strNum = parseInt(numbers[0] + numbers[numbers.length-1])
+      console.log(`Line number: ${strNum}`)
+      result += strNum
+    }
+    else {
+      console.log(red + `This line has no numbers: ${l}` + reset)
+    }
+  }
+
+  return result
+}
+
+function parttwo(data){
+  console.log(yellow + "Part Two" + reset)
+  var newData = cleanString(data)
+  newData = cleanString(newData)
+
+  return partone(newData)
+}
+
+function cleanString(data) {
+  const newData = data.replace(/(one|two|three|four|five|six|seven|eight|nine)/gi, (m) => {
+    const wordToNumberMap = {
+      zero: 'z0o',
+      one: 'o1e',
+      two: 't2o',
+      three: 't3e',
+      four: 'f4r',
+      five: 'f5e',
+      six: 's6x',
+      seven: 's7n',
+      eight: 'e8t',
+      nine: 'n9e'
+    };
+    const replacement = wordToNumberMap[m.toLowerCase()];
+    // console.log(`OG: ${m} Replacement: ${replacement}`)
+    return replacement !== undefined ? replacement : m;
+  })
+  return newData
 }
 
 function displayResults(data){
