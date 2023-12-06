ca8eba6f5e13376ecf0a299866cbc2bd35212be8
Timothy Dowling <timothy.dowling@me.com>
2023-12-06 16:28:25 -0500

    Day 4 start

--- 2023/Days/Day4/day4.py
+++ 2023/Days/Day4/day4.py
@@ -1,4 +1,4 @@
-with open('test.txt', 'r') as file:
+with open('data.txt', 'r') as file:
     lines = file.readlines()
 
 games_won = {}
@@ -10,26 +10,39 @@ for line in lines:
     winners = line.split(':')[1].split(' | ')[0].strip().split(' ')
     picks = list(filter(lambda s: s != '', line.split(':')[1].split(' | ')[1].split(' ')))
 
+    print(card)
+    print(winners)
+    print(picks)
+
     copies = 0
-    if int(card) in games_won:
-        copies = games_won[int(card)]
+    if card in games_won:
+        copies = games_won[card]
 
     matches = 0
+    for pick in picks:
+        for winner in winners:
+            if pick == winner:
+                matches += 1
+
+    if card in games_won:
+        games_won[card] += 1
+    else:
+        games_won[card] = 1
+    # print(f"og card: {card} | Won so far: {games_won[int(card)]}")
+
+    # print(f"{copies} copies of Card {card}")
     for i in range(copies + 1):
-        for pick in picks:
-            for winner in winners:
-                if pick == winner:
-                    matches += 1
-        
-        for m in range(int(card), matches + int(card) + 1):
+        for m in range(int(card) + 1, matches + (card + 1)):
             if m in games_won:
                 games_won[m] += 1
             else:
                 games_won[m] = 1
+            # print(f"card: {m} | {games_won[m]}")
 
 result = 0
 for game in games_won:
-    result += game
+    # print(game)
+    result += int(games_won[game])
 
 print(games_won)
 
