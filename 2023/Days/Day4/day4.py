422846dddc576f0df6ae688fa19a7ff5b8cfb22b
Timothy Dowling <timothy.dowling@me.com>
2023-12-07 12:55:46 -0500

    Day 4 Completed

--- 2023/Days/Day4/day4.py
+++ 2023/Days/Day4/day4.py
@@ -6,13 +6,14 @@ games_won = {}
 for line in lines:
     line = line.replace('\n', '')
 
-    card = line.split(':')[0].split(' ')[1]
+    card = int(line.split(':')[0].split(' ')[-1])
     winners = line.split(':')[1].split(' | ')[0].strip().split(' ')
     picks = list(filter(lambda s: s != '', line.split(':')[1].split(' | ')[1].split(' ')))
 
-    print(card)
-    print(winners)
-    print(picks)
+    # print(f"line:{line}")
+    # print(f"card:{card}")
+    # print(f"winners:{winners}")
+    # print(f"picks:{picks}")
 
     copies = 0
     if card in games_won:
@@ -28,16 +29,25 @@ for line in lines:
         games_won[card] += 1
     else:
         games_won[card] = 1
-    # print(f"og card: {card} | Won so far: {games_won[int(card)]}")
+    # print(f"Card: {card} | Won so far: {games_won[card]}")
+
+    if matches > 0:
+        for m in range(card + 1, matches + card + 1):
+            if m in games_won:
+                games_won[m] += 1
+            else:
+                games_won[m] = 1
+            # print(f"og win - card: {m} | {games_won[m]}")
 
     # print(f"{copies} copies of Card {card}")
-    for i in range(copies + 1):
-        for m in range(int(card) + 1, matches + (card + 1)):
+    for i in range(copies):
+        for m in range(card + 1, matches + card + 1):
             if m in games_won:
                 games_won[m] += 1
             else:
                 games_won[m] = 1
             # print(f"card: {m} | {games_won[m]}")
+    # print("----------------------------")
 
 result = 0
 for game in games_won:
